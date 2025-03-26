73a8a92fe6f2e16b3a9699991e1ac4b0cd7b6885
Vincent Wong <vvcent@gmail.com>
2025-03-26 18:49:36 +0800

    🔧chore: fix build error on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+
+// Get the directory name using URL
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
