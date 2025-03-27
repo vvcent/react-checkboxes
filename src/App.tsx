9607b64ad557e7020f43a8b8ef442813b2104f4c
Vincent Wong <vvcent@gmail.com>
2025-03-27 13:48:30 +0800

    ✨feat: update demo options

--- src/App.tsx
+++ src/App.tsx
@@ -61,46 +61,10 @@ const App = () => {
 					columns: 5,
 				}}
 				options={[
-					{
-						label: 'Option 1',
-						value: 'option1',
-					},
-					{
-						label: 'Option 2',
-						value: 'option2',
-					},
-					{
-						label: 'Option 3',
-						value: 'option3',
-					},
-					{
-						label: 'Option 4',
-						value: 'option4',
-					},
-					{
-						label: 'Option 5',
-						value: 'option5',
-					},
-					{
-						label: 'Option 6',
-						value: 'option6',
-					},
-					{
-						label: 'Option 7',
-						value: 'option7',
-					},
-					{
-						label: 'Option 8',
-						value: 'option8',
-					},
-					{
-						label: 'Option 9',
-						value: 'option9',
-					},
-					{
-						label: 'Option 10',
-						value: 'option10',
-					},
+					...Array.from({ length: 500 }, (_, index) => ({
+						label: `Option ${index + 1}`,
+						value: `option${index + 1}`,
+					})),
 				]}
 				defaultSelected={['option1', 'option2', 'option5', 'option7']}
 				onChange={(selectedValues) => {
